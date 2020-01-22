fbd50164de13c284d10002f863ba5c631347f9a7
Mai Zeng <mai.zeng@mail.mcgill.ca>
2020-01-22 16:33:46 -0500

    added a2 and .gitignore

--- A2/pipeline.vhd
+++ A2/pipeline.vhd
@@ -11,10 +11,33 @@ end pipeline;
 
 architecture behavioral of pipeline is
 
+signal op1_final, op2_final, op3_final, op4_final, op5_final : integer;
+signal op1_processing, op2_processing, op3_processing, op4_processing, op5_processing : integer;
+
 begin
 -- todo: complete this
 process (clk)
 begin
+  if rising_edge(clk) then
+    op1_final <= op1_processing;
+    op2_final <= op2_processing;
+    op3_final <= op3_processing;    
+    op4_final <= op4_processing;    
+    op5_final <= op5_processing;  
+  end if;  
 end process;
+
+op1_processing <= a+b;
+op2_processing <= op1_final * 42;
+op3_processing <= c*d;
+op4_processing <= a-e;
+op5_processing <= op3_final * op4_final;
+final_output <= op2_final - op5_final;
+
+op1 <= op1_final;
+op2 <= op2_final;
+op3 <= op3_final;
+op4 <= op4_final;
+op5 <= op5_final;
 
 end behavioral;
--- A2/pipeline_tb.vhd
+++ A2/pipeline_tb.vhd
@@ -38,6 +38,79 @@ END PROCESS;
 stim_process: PROCESS
 BEGIN   
 	--TODO: Stimulate the inputs for the pipelined equation ((a + b) * 42) - (c * d * (a - e)) and assert the results
+	REPORT "Fill the pipeline and wait for 3 clock cycle";
+	s_a <= 1;
+	s_b <= 2;
+	s_c <= 3;
+	s_d <= 4;
+	s_e <= 5;
+	
+	WAIT FOR 3 * clk_period;
+	
+	ASSERT (s_op1 = 3) 
+	REPORT "op1 should = 0 but was :" & integer'image(s_op1) SEVERITY ERROR;
+  ASSERT (s_op3 = 12) 
+  REPORT "op3 should = 0 but was :" & integer'image(s_op1) SEVERITY ERROR;
+  ASSERT (s_op4 = -4) 
+  REPORT "op4 should = 0 but was :" & integer'image(s_op1) SEVERITY ERROR;
+  ASSERT (s_op2 = 126) 
+  REPORT "op2 should = 0 but was :" & integer'image(s_op1) SEVERITY ERROR;
+  ASSERT (s_op5 = -48) 
+  REPORT "op1 should = but was :0" & integer'image(s_op1) SEVERITY ERROR; 
+  ASSERT (s_final_output = 174) 
+  REPORT "final output should = 174 after 2 CC, but was " & integer'image(s_final_output) SEVERITY ERROR;
+	
+	REPORT "-------------------------------------------------------------------------------";
+	
+	REPORT "Fill the pipeline and wait for 1 clock cycle should get 11, 13, 14";
+	s_a <= 1;
+	s_b <= 2;
+	s_c <= 3;
+	s_d <= 4;
+	s_e <= 5;
+	
+	ASSERT (s_op1 = 3) 
+	REPORT "op1 should = 0 but was :" & integer'image(s_op1) SEVERITY ERROR;
+  ASSERT (s_op3 = 12) 
+  REPORT "op3 should = 0 but was :" & integer'image(s_op1) SEVERITY ERROR;
+  ASSERT (s_op4 = -4) 
+  REPORT "op1 should = -4 after 1 CC, but was " & integer'image(s_op1) SEVERITY ERROR; 
+
+ 	WAIT FOR 1* clk_period;
+ 	
+ 	REPORT "Fill the another pipeline and wait for 1 clock cycle should get 12, 15, 21, 23, 24";
+ 	s_a <= 2;
+	s_b <= 3;
+	s_c <= 4;
+	s_d <= 5;
+	s_e <= 6;
+	
+	ASSERT (s_op2 = 126) 
+  REPORT "op2 should = 0 but was :" & integer'image(s_op1) SEVERITY ERROR;
+  ASSERT (s_op5 = -48) 
+  REPORT "op1 should = but was :0" & integer'image(s_op1) SEVERITY ERROR; 
+  ASSERT (s_op1 = 3) 
+  REPORT "op1 should = 5 after 2 CC, but was " & integer'image(s_op1) SEVERITY ERROR;
+  ASSERT (s_op3 = 12) 
+  REPORT "op3 should = 20 after 2 CC, but was " & integer'image(s_op3) SEVERITY ERROR;
+  ASSERT (s_op4 = -4) 
+  REPORT "op4 should = -4 after 2 CC, but was " & integer'image(s_op4) SEVERITY ERROR;
+  ASSERT (s_final_output = 174) 
+	REPORT "final output should = 174 after 2 CC, but was " & integer'image(s_final_output) SEVERITY ERROR;
+  
+ 	WAIT FOR 1* clk_period;
+	ASSERT (s_op1 = 5) 
+  REPORT "op1 should = 5 after 2 CC, but was " & integer'image(s_op1) SEVERITY ERROR;
+  ASSERT (s_op3 = 20) 
+  REPORT "op3 should = 20 after 2 CC, but was " & integer'image(s_op3) SEVERITY ERROR;
+  ASSERT (s_op4 = -4) 
+  REPORT "op4 should = -4 after 2 CC, but was " & integer'image(s_op4) SEVERITY ERROR;
+	ASSERT (s_op2 = 126) 
+  REPORT "op2 should = 0 but was :" & integer'image(s_op1) SEVERITY ERROR;
+  ASSERT (s_op5 = -48) 
+  REPORT "op1 should = but was :0" & integer'image(s_op1) SEVERITY ERROR; 
+  ASSERT (s_final_output = 174) 
+	REPORT "final output should = 174 after 2 CC, but was " & integer'image(s_final_output) SEVERITY ERROR;
 	WAIT;
 END PROCESS stim_process;
 END;
